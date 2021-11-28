5c930f4dbf5527fc05c77ffa94914834d24bdd71
Torbatti <torbattii@gmail.com>
2021-11-28 21:57:05 +0330

    Added Rust Cheat Sheet

---
+++ rust/all_in_one.rs
@@ -0,0 +1,47 @@
+
+/* What IS Rust? (Start)*/
+
+
+
+/* What IS Rust? (End)*/
+
+/*  Installing Rust (Start)*/
+    // For Mac And Linux 
+        /* 
+        1)Type the following command in terminal
+            $ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+        2)Youll also need to download a C compiler which inculde a linker
+            Gnu/Linux)
+                Debian Based Distros)
+                    sudo apt install build-essentials
+
+                Arch Based Distros) 
+                    sudo pacman -S base-devel
+
+                Fedora and other dnf based package managers disros)
+                    sudo dnf install make automake gcc gcc-c++ kernel-devel
+
+            MacOS)
+                IDK ? I dont have one and you shouldnt too, just kidding
+                type the following command itll install a c compiler for you
+                    xcode-select --install
+        */
+    // For windows
+        /*
+            go here , i guess
+                https://forge.rust-lang.org/infra/other-installation-methods.html
+        */
+/* Installing Rust (End)*/
+
+/* Common Programming Concepts (Start)*/
+
+
+
+//  Command Line
+/*
+    Command Block
+*/
+
+
+
+/* Common Programming Concepts (End)*/
