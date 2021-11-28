6226f1afd9e953759f0f614640c00c0f92d5c368
Torbatti <torbattii@gmail.com>
2021-11-28 22:52:38 +0330

    Added resources

--- rust/all_in_one.rs
+++ rust/all_in_one.rs
@@ -1,47 +1,86 @@
+/*
+    Check resources.rs for some great resources to learn rust
+*/
 
+
+
+
+/***************************************/
 /* What IS Rust? (Start)*/
 
 
 
 /* What IS Rust? (End)*/
-
+/***************************************/
 /*  Installing Rust (Start)*/
     // For Mac And Linux 
         /* 
-        1)Type the following command in terminal
+        1)Type the following command in your terminal
             $ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
-        2)Youll also need to download a C compiler which inculde a linker
+
+        2)Youll also need to download a C compiler which will inculde a linker
             Gnu/Linux)
                 Debian Based Distros)
-                    sudo apt install build-essentials
+                    $ sudo apt install build-essentials
 
                 Arch Based Distros) 
-                    sudo pacman -S base-devel
+                    $ sudo pacman -S base-devel
 
                 Fedora and other dnf based package managers disros)
-                    sudo dnf install make automake gcc gcc-c++ kernel-devel
+                    $ sudo dnf install make automake gcc gcc-c++ kernel-devel
 
             MacOS)
                 IDK ? I dont have one and you shouldnt too, just kidding
                 type the following command itll install a c compiler for you
-                    xcode-select --install
+                    $ xcode-select --install
         */
+
     // For windows
         /*
             go here , i guess
                 https://forge.rust-lang.org/infra/other-installation-methods.html
         */
-/* Installing Rust (End)*/
 
+/* Installing Rust (End)*/
+/***************************************/
+/* Using Cargo To build and run (Start)*/
+    /*
+        //make a new project
+            1)make a new project in a new folder type the following command
+                $ cargo new nameOfYourProject
+
+            2)make a new project in the current directory(folder) type the following command
+                $ cargo init
+
+        //build
+            //TODO
+
+        //build and run 
+            to build and run your project type the following command
+                $ cargo run
+
+        //Check
+            //TODO
+
+
+    */
+/* Using Cargo To build and run (End)*/
+/***************************************/
 /* Common Programming Concepts (Start)*/
 
+    /* Comments(Start)*/
 
+        //  Command Line (anything after // is commented)
 
-//  Command Line
-/*
-    Command Block
-*/
+        /*
+            Command Block (anything between is commented)
+        */
 
+    /* Comments(End)*/
 
+    /* Variables, Constants , Mutation(Start)*/
+
+    /* Variables, Constants , Mutation(End)*/
 
 /* Common Programming Concepts (End)*/
+/***************************************/
---
+++ rust/resources.rs
@@ -0,0 +1,40 @@
+/*
+Some Really Good Resources You can Learn From (Basics to Advanced Concepts):
+
+examples/book:
+    Rust Book(the book): (i )
+        https://doc.rust-lang.org/book/
+
+    Rust By Examples: (its a book) (i used this one to learn rust it has better documentations than the book)
+        https://doc.rust-lang.org/stable/rust-by-example/
+
+    RustLaings Course: (Code And Learn)
+        https://github.com/rust-lang/rustlings/
+
+
+
+rusty youtube Channels:
+    lets get rusty : (EVERRYTHING ABOUT RUST , GREAT!)
+        https://www.youtube.com/channel/UCSp-OaMpsO8K0KkOqyBl7_w
+
+    TanTan: (Rust in Games Pretty Xciting)
+        https://www.youtube.com/channel/UChl_NKOs1qqh_x7yJfaDpDw
+
+    Jon Gjengset : (great channel for intermediate+ rust learners)
+        https://www.youtube.com/channel/UC_iD0xppBwwsrM9DegC5cQQ
+
+some good rusty tutorials:
+
+    Introduction to Rust with Brooks Builds - Build a CLI Todo App : 
+        https://youtu.be/IhvzGzoq4mc
+
+
+
+some really good rust lectures:
+    Stanford Seminar The Rust Programming Language - The Best Documentary Ever : (Damn I love this One Its great and gives examples about ownership , borrowing , etc ..)
+        https://youtu.be/SZvs15hC81U
+
+    RustConf 2019 - From Electron, to Wasm, to Rust (Aaand Back to Electron) by Irina Shestak: (ngl she has the craziest presentaion slides ever beautiful)
+        https://youtu.be/lLzFJenzBng
+
+*/
