8c60f0cdbd605332aee19f55afb2853a8d58f249
Torbatti <torbattii@gmail.com>
2022-04-23 20:28:12 +0430

    +more reorganizations

--- js/_N_online_game_sync.js
+++
@@ -1,7 +0,0 @@
-// 
-/* 
-    Zed:"if you're using JS, use WebSockets and something more lightweight than JSON"
-    https://msgpack.org/
-    A binary representation of JSON data
-    JSON, but much, much smaller
-*/
--- rust/_N_str_and_String.rs
+++
@@ -1,3 +0,0 @@
-// &str And String types are different
-let name_1 = "Arya"; // this is &str 
-let name_2 = String::from("Arya"); // this is string 
